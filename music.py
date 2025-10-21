1ef1faf86d76a9e07c0dc25e8d7b9af7c5a2aa4a
Lastixs <lastix12s@gmail.com>
2025-10-21 18:30:20 +0300

    Update music.py

--- music.py
+++ music.py
@@ -68,7 +68,6 @@ async def download_audio(video_url: str) -> str:
         'retries': 10,  # количество попыток при ошибке сети
         'socket_timeout': 15,  # таймаут соединения
         'nocheckcertificate': True,  # пропустить SSL-проверку
-        'proxy': 'socks5://fEORJNiFkX:LkWCF4HDnQ@194.31.73.130:46180',  # <--- вставь свой прокси сюда
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
